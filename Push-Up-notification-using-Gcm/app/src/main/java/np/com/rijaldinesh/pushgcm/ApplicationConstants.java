4bfe485e33adfa403450c20db9eb357585599b51
dineshrijal119 <dineshrijal119@gmail.com>
2016-03-03 17:30:22 +0545

    adding google cloud messaging

---
+++ Push-Up-notification-using-Gcm/app/src/main/java/np/com/rijaldinesh/pushgcm/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package np.com.rijaldinesh.pushgcm;
+
+public interface ApplicationConstants {
+
+	// Php Application URL to store Reg ID created
+	static final String APP_SERVER_URL = "http://10.10.10.49:80/gcm.php?shareRegId=true";
+
+	// Google Project Number
+	static final String GOOGLE_PROJ_ID = "22771634903";
+	// Message Key
+	static final String MSG_KEY = "m";
+
+}
